dfab858262b4c1c1c7154bcf2e878385979f18ab
Chaoran Wang <showcy@vt.edu>
2022-01-21 01:38:45 -0800

    Add docstring for corr_heatmap.

---
+++ R/collinearityR.R
@@ -0,0 +1,16 @@
+corr_heatmap <- function(df){
+  #' Plot rectangular data as a color-encoded Pearson correlaiton matrix.
+  #' 
+  #' @description The rows and the columns contain variable names, while 
+  #' the heatmap tiles contain Pearson correlation coefficient and 
+  #' corresponding colours.
+  #' 
+  #' @param df dataframe. 2-D dataframe.
+  #' 
+  #' @usage corr_heatmap(df)
+  #' @return The heatmap.
+  #' @details The inputs must be a 2-D data frame
+  #' @examples
+  #' corr_heatmap(data)
+
+}
