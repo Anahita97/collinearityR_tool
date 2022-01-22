6ac50ddc570d7405c2211c60733a1ed84bc993a3
Chaoran Wang <showcy@vt.edu>
2022-01-21 20:16:42 -0800

    Move docstring before function.

--- R/collinearityR.R
+++ R/collinearityR.R
@@ -1,17 +1,17 @@
+#' Plot rectangular data as a color-encoded Pearson correlaiton matrix.
+#' 
+#' @description The rows and the columns contain variable names, while 
+#' the heatmap tiles contain Pearson correlation coefficient and 
+#' corresponding colours.
+#' 
+#' @param df dataframe. 2-D dataframe.
+#' 
+#' @usage corr_heatmap(df)
+#' @return The heatmap.
+#' @export
+#' @details The inputs must be a 2-D data frame
+#' @examples
+#' corr_heatmap(data)
 corr_heatmap <- function(df){
-  #' Plot rectangular data as a color-encoded Pearson correlaiton matrix.
-  #' 
-  #' @description The rows and the columns contain variable names, while 
-  #' the heatmap tiles contain Pearson correlation coefficient and 
-  #' corresponding colours.
-  #' 
-  #' @param df dataframe. 2-D dataframe.
-  #' 
-  #' @usage corr_heatmap(df)
-  #' @return The heatmap.
-  #' @export
-  #' @details The inputs must be a 2-D data frame
-  #' @examples
-  #' corr_heatmap(data)
 
 }
