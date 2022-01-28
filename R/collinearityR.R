ad7eb88f362f0f16ff0847ff7a267d650cb2ca3f
nickmao1994 <nickmao1994@gmail.com>
2022-01-28 12:09:00 -0800

    Create basic functions

--- R/collinearityR.R
+++ R/collinearityR.R
@@ -11,4 +11,19 @@
 #' @examples
 #' corr_matrix(tibble(x = 1:5, y = 2:6))
 
-corr_matrix <- function(df, decimals = 2) {}
+library(tidyverse)
+
+corr_matrix <- function(df, decimals = 2) {
+    corr_matrix <- df |> 
+        select_if(is.numeric) |> 
+        cor(method = "pearson") |> 
+        round(digits = decimals)
+
+
+    corr_matrix_longer <- corr_matrix |> 
+        as.data.frame() |> 
+        rownames_to_column("var1") |>
+        pivot_longer(-var1, names_to = "var2", values_to = "corr")
+    
+    result <-  list(corr_matrix, corr_matrix_longer)
+}
