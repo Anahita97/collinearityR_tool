af06517e6774861185684c388dd390e4f9153687
Katia Aristova <k.aristova@gmail.com>
2022-01-28 21:54:06 -0800

    Run checks and update dependecies

--- R/collinearityR.R
+++ R/collinearityR.R
@@ -1,6 +1,6 @@
 #' Calculate the Pearson correlation coefficients for all numeric variables.
 #' Round the outcome to desired decimals.
-#'    
+#'
 #' @param df tibble, the input dataframe
 #'
 #' @param decimals int, the number of decimals
@@ -9,23 +9,21 @@
 #' that can be passed to other functions in this package
 #'
 #' @examples
-#' corr_matrix(tibble(x = 1:5, y = 2:6))
 corr_matrix <- function(df, decimals = 2) {}
 
 #' Plot rectangular data as a color-encoded Pearson correlaiton matrix.
-#' 
-#' @description The rows and the columns contain variable names, while 
-#' the heatmap tiles contain Pearson correlation coefficient and 
+#'
+#' @description The rows and the columns contain variable names, while
+#' the heatmap tiles contain Pearson correlation coefficient and
 #' corresponding colours.
-#' 
+#'
 #' @param df dataframe. 2-D dataframe.
-#' 
+#'
 #' @usage corr_heatmap(df)
 #' @return The heatmap.
 #' @export
 #' @details The inputs must be a 2-D data frame
 #' @examples
-#' corr_heatmap(data)
 corr_heatmap <- function(df){}
 
 #' Returns a list containing a dataframe that includes Variance Inflation Factor (VIF) score and
@@ -41,27 +39,27 @@ corr_heatmap <- function(df){}
 #' @export
 #'
 #' @examples
-#' vif_bar_plot(["exp1", "exp2", "exp3"], "response", data, 5)
 vif_bar_plot <- function(x, y, df, thresh){
 }
 
-#' Multicollinearity identification function highly correlated pairs 
+#' Multicollinearity identification function highly correlated pairs
 #' (Pearson coefficient) with VIF values exceeding the threshold.
-
+#'
 #' This function returns a DataFrame containing Pearson's coefficient,
 #' VIFF with explanatory variables suggested for elimination.
 #' An empty dataframe means no multicollinearity detected.
 #'
 #' @param df An input dataframe
-#' @param corr_min A decimal number that serves as a threshold for selecting
+#' @param X Explanatory variables (vector of characters)
+#' @param y Response variable (single vector)
+#' @param corr_min (optional) A decimal number that serves as a threshold for selecting
 #' a pair. This is a Pearson coefficient value. Default set at -0.8.
-#' @param corr_max A decimal number that serves as a threshold for selecting
+#' @param corr_max (optional) A decimal number that serves as a threshold for selecting
 #' a pair. This is a Pearson coefficient value. Default set at 0.8.
-#' @param vif_limit A decimal number that serves as a threshold for selecting
+#' @param vif_limit (optional) A decimal number that serves as a threshold for selecting
 #' a pair. This is a VIF value. Default set at 4.
 #' @export
 #' @examples
-#' col_identify(df, X, y)
 col_identify <- function(
     df, X, y, corr_min = -0.8, corr_max = 0.8, vif_limit = 4) {
 
@@ -80,46 +78,45 @@ col_identify <- function(
     if (!is.numeric(corr_min)) {
          stop("corr_min should be numeric")
     }
-    
+
     if (!is.numeric(corr_max)) {
          stop("corr_max should be numeric")
     }
-    
+
     if (!is.numeric(vif_limit)) {
          stop("vif_limit should be numeric")
     }
 
     input_corr <- df |>
-        select(all_of(X))
+        dplyr::select(tidyselect::all_of(X))
 
     input_corr <- corr_matrix(input_corr)[[1]] |>
-        filter(
+        dplyr::filter(
             correlation <= corr_min |
             correlation >= corr_max &
             variable1 != variable2)
 
     pair_maker <- function(x, y) {
         list_vector <- c(x, y) |>
-        str_sort()
+        stringr::str_sort()
         list_vector
         }
 
-    input_corr$pair <- map2(input_corr$variable1,
+    input_corr$pair <- purrr::map2(input_corr$variable1,
                             input_corr$variable2,
                             pair_maker)
 
     vif_output <- vif_bar_plot(X, y, df, 4)[[1]] |>
-        rename(variable1 = explanatory_var)
+        dplyr::rename(variable1 = explanatory_var)
 
-    results_df <- inner_join(input_corr, vif_output) |>
-        arrange(desc(pair)) |>
-        select(-variable2) |>
-        rename(variable = variable1) |>
-        group_by(pair) |>
-        top_n(1, vif_score)
+    results_df <- dplyr::inner_join(input_corr, vif_output) |>
+        dplyr::arrange(desc(pair)) |>
+        dplyr::select(-variable2) |>
+        dplyr::rename(variable = variable1) |>
+        dplyr::group_by(pair) |>
+        dplyr::top_n(1, vif_score)
 
     results_df
 }
-
 
 
---
+++ tests/testthat/test-col_identify.R
@@ -0,0 +1,46 @@
+testthat::test_that("col_identify should return a datafame", {
+
+  mpg <- system.file("extdata", "mpg.csv", package="collinearityR")
+  real_df <- read.csv(mpg)
+  exp <- c("cyl", "cty", "displ", "year")
+  resp <- c("hwy")
+  expect_true("data.frame" %in% c(class(col_identify(real_df, exp, resp))))
+})
+
+
+testthat::test_that('function should throw an error if the arguments are the wrong type', {
+
+  fake_df <- c("apples", "bananas")
+  fake_exp <- 100
+  fake_resp <- c(100, 200)
+  mpg <- system.file("extdata", "mpg.csv", package="collinearityR")
+  real_df <- read.csv(mpg)
+  exp <- c("cyl", "cty", "displ", "year")
+  resp <- c("hwy")
+
+  expect_error(col_identify(fake_df, exp, resp))
+  expect_error(col_identify(real_df, fake_exp, resp))
+  expect_error(col_identify(real_df, exp, fake_resp))
+})
+
+
+testthat::test_that("col_identify should return correct matches", {
+
+  variable <- c("cyl", "cyl")
+  correlation <- c(0.9302, -0.8058)
+  rounded_corr <- c(0.93, -0.81)
+  pair <- list(c("cyl", "displ"), c("cty", "cyl"))
+  vif_score <- c(8.0817, 8.0817)
+
+  mpg <- system.file("extdata", "mpg.csv", package="collinearityR")
+  real_df <- read.csv(mpg)
+  exp <- c("cyl", "cty", "displ", "year")
+  resp <- c("hwy")
+
+  toy_df_col <- tibble::tibble(variable, correlation, rounded_corr, pair, vif_score)
+  col_output <- col_identify(real_df, exp, resp) |>
+    dplyr::mutate(correlation = round(correlation, 4),
+                  vif_score = round(vif_score, 4))
+
+  expect_equivalent(col_output, toy_df_col)
+})
