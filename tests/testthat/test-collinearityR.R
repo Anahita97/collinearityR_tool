789d20a0cc4f393af6b1c414e08c010ba7de3c05
Chaoran Wang <showcy@vt.edu>
2022-01-28 18:26:43 -0800

    Finish tests for corr_heatmap.

--- tests/testthat/test-collinearityR.R
+++ tests/testthat/test-collinearityR.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+data_1 <-  data.frame('A'=c(1,2,3,4,5),
+                      'B'=c(2,4,6,8,10),
+                      'C'=c(10,8,6,4,2),
+                      "D"=c('abc','322', '324', '32', '23')
+)
+
+test_that('Base layer of heatmap should use geom_tile and map correlaiton to fill property.', {
+  expect_true("GeomTile" %in% c(class(corr_heatmap(data_1)$layers[[1]]$geom)))
+  expect_true("correlation" == rlang::get_expr(corr_heatmap(data_1)$mapping$fill))
+})
+
+test_that('Text layer of heatmap should use geom_text.', {
+  expect_true("GeomText" %in% c(class(corr_heatmap(data_1)$layers[[2]]$geom)))
 })
